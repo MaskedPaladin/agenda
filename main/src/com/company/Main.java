d3d6e738c18bc64a383c1bee66df9b8fd0ae0efd
MaskedPaladin <carjien@gmail.com>
2021-03-24 16:56:36 +0100

    .

---
+++ main/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+
+
+class Memory{
+}
+class Person{
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Memory mp = new Memory();
+    }
+}
