42f73a063fef102b267aea2817400c61e1c1116e
MaskedPaladin <carjien@gmail.com>
2021-04-11 21:24:06 +0200

    .

---
+++ warsim/src/com/company/Member.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Member {
+    String name;
+    int str;
+    int intel;
+    int dex;
+    int con;
+    int wisp;
+    float media;
+    int hp;
+    float mana;
+    float stamina;
+    Member() {
+        stamina = (str * con)/1.2F;
+        name = name;
+        str = str;
+        intel = intel;
+        dex = dex;
+        con = con;
+        wisp = wisp;
+        hp = hp;
+        mana = mana;
+        stamina = stamina;
+        media = media;
+    }
+}
