73ca70cc47ee1f334b49bc266f50e70d6bda016a
MaskedPaladin <carjien@gmail.com>
2021-04-11 20:07:31 +0200

    .

--- main/src/com/company/Main.java
+++ main/src/com/company/Main.java
@@ -2,14 +2,76 @@ package com.company;
 import java.util.*;
 import java.io.*;
 
-
-class Memory{
-    String a = new String();
-}
 class Person{
-    String b = new String();
+    int id;
+    String name;
+    String surname;
+    float mass;
+    int age;
+    Dictionary weapons = new Hashtable();
+    Person(int id, String name, String surname, float mass, int age){
+        id= id;
+        name = name;
+        surname = surname;
+        mass = mass;
+        age = age;
+    }
+    public static void draw(Person a){
+        int id = a.id;
+        String name = a.name;
+        String surname = a.surname;
+        float mass = a.mass;
+        int age = a.age;
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(mass);
+        System.out.println(age);
+    }
 }
+
 public class Main {
+    static int nitems = 0;
+    static void add(Person list, String field, String value){
+        for (int i = 0; i < nitems; i++) {
+
+        }
+    }
+    static void command(){
+        Scanner c = new Scanner(System.in);
+        System.out.print(">>> ");
+        String com = c.next();
+        if (com.contentEquals("add")){
+            add(persons, 10, "");
+        }
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < persons.length; i++) {
+            persons[i] = new Person(0, null, null, 0, 0);
+            nitems++1;
+        }
+        for (int i = 0; i < persons.length; i++) {
+            System.out.println(i);
+            System.out.print("Name: ");
+            String name = sc.next();
+            persons[i].name = name;
+            System.out.println();
+            System.out.print("Surname: ");
+            String surname = sc.next();
+            persons[i].surname = surname;
+            System.out.println();
+            System.out.print("Mass: ");
+            float mass = sc.nextFloat();
+            persons[i].mass = mass;
+            System.out.println();
+            System.out.print("Age: ");
+            int age = sc.nextInt();
+            persons[i].age = age;
+        }
+        System.out.println(persons[0].age);
+        for (int i = 0; i < persons.length; i++) {
+            Person.draw(persons[i]);
+        }
     }
 }
---
+++ untitled/src/com/company/GameFuncs.java
@@ -0,0 +1,86 @@
+package com.company;
+import java.util.Scanner;
+
+public class GameFuncs{
+    public void menu(){
+        System.out.println("||||||||||");
+        System.out.println("WAR SIM");
+        System.out.println("**********");
+        System.out.println("1. Jugar");
+        System.out.println("2. Editar");
+        System.out.println("3. Listar");
+        System.out.println("4. Salir");
+    }
+    public void pedir(){
+        Scanner sc = new Scanner(System.in);
+        menu();
+        int input = sc.nextInt();
+        if (input==1){
+            jugar();
+            pedir();
+        }
+        if (input==2){
+            editar();
+            pedir();
+        }
+        if (input==3){
+            listar();
+            pedir();
+        }
+        if (input==4){
+            System.exit(0);
+        }
+        if (input > 3 || input < 0){
+            System.out.println("Not valid value:");
+            System.out.println("1. Jugar");
+            System.out.println("2. Editar");
+            System.out.println("3. Listar");
+            System.out.println("4. Salir");
+            pedir();
+        }
+    }
+    int maxmembers = 4;
+    Member[] enemies = new Member[maxmembers];
+    Member[] allied = new Member[maxmembers];
+    void initialize(){
+        for (int i = 0; i < maxmembers; i++) {
+            allied[i] = new Member();
+            enemies[i] = new Member();
+        }
+    }
+    public void jugar(){
+    }
+    public void editar(){
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < maxmembers; i++) {
+            System.out.println("Slot: "+(i));
+            System.out.print("Nombre: ");
+            String nombre = sc.next();
+            System.out.print("Strength: ");
+            int strength = sc.nextInt();
+            System.out.print("Intellect: ");
+            int intellect = sc.nextInt();
+            System.out.print("Dexerty: ");
+            int dexerty = sc.nextInt();
+            System.out.print("Constitution: ");
+            int constitution = sc.nextInt();
+            System.out.print("Wispower: ");
+            int wispower = sc.nextInt();
+            allied[i].name = nombre;
+            allied[i].str = strength;
+            allied[i].intel = intellect;
+            allied[i].dex = dexerty;
+            allied[i].con = constitution;
+            allied[i].wisp = wispower;
+            allied[i].media = (strength+intellect+dexerty+constitution+wispower)/5;
+            allied[i].hp = (strength*constitution)*4;
+            allied[i].mana = intellect*2.3f;
+            allied[i].stamina = (strength+constitution)/1.6f;
+        }
+    }
+    public void listar(){
+        for (int i = 0; i < maxmembers; i++) {
+            System.out.println("Name: "+allied[i].name+" HP: "+allied[i].hp+" MANA: "+allied[i].mana+" STAMINA: "+allied[i].stamina+" (STR: "+allied[i].str+" INT: "+allied[i].intel+" DEX: "+allied[i].dex+" CON: "+allied[i].con+" WIS: "+allied[i].wisp+" MEDIA: "+allied[i].media+")");
+        }
+    }
+}
