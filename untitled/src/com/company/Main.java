72ce6a55161c289c83f567e79a459e804eaf4d8d
MaskedPaladin <carjien@gmail.com>
2021-04-11 20:53:43 +0200

    Delete untitled directory

--- untitled/src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-import java.util.Scanner;
-public class Main {
-    public static GameFuncs game = new GameFuncs();
-    public static void main(String[] args) {
-        game.initialize();
-        game.pedir();
-    }
-}
